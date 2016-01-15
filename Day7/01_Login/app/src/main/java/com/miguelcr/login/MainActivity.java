bb03c54b3aecd3dfcc9ed5e5c04e53b172d3e31c
Miguel Campos <camposmiguel@gmail.com>
2016-01-15 16:34:02 +0100

    Day7

--- Day7/01_Login/app/src/main/java/com/miguelcr/login/MainActivity.java
+++ Day7/01_Login/app/src/main/java/com/miguelcr/login/MainActivity.java
@@ -1,5 +1,7 @@
 package com.miguelcr.login;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +10,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences settings = getSharedPreferences("FRIENDER_PREF", 0);
+        boolean isLogged = settings.getBoolean("login", false);
+
+        if(isLogged) {
+            // Go directly to the HomeActivity
+            Intent i = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(i);
+        }
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,6 +43,45 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // findViewByID...
+        btnLogin = (Button)findViewById(R.id.buttonLogin);
+        email = (EditText)findViewById(R.id.editTextEmail);
+        password = (EditText)findViewById(R.id.editTextPassword);
+
+        // setOnClickListener
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.getText().toString().equals("email@email.com")
+                        && password.getText().toString().equals("1234")) {
+
+                    // Save the data
+                    SharedPreferences settings = getSharedPreferences("FRIENDER_PREF", 0);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString("email",email.getText().toString());
+                    editor.putBoolean("login",true);
+
+                    // Commit the edits!
+                    editor.commit();
+
+                    // Reset the values of the form elements
+                    email.setText("");
+                    password.setText("");
+
+                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(i);
+
+                    // Destroy this Activity because if someone go BACK we don't want to show
+                    // the login page
+                    MainActivity.this.finish();
+                } else {
+                    Toast.makeText(MainActivity.this, "Incorrect login. Try again!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 
     @Override
@@ -49,4 +105,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
